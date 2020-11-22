b51100227026d20ccffae1f2740546f2969078c9
MerlinEgalite <egalite.merlin@gmail.com>
2020-11-22 11:10:41 +0100

    add update eth price

--- caller/contracts/CallerContract.sol
+++ caller/contracts/CallerContract.sol
@@ -7,12 +7,20 @@ contract CallerContract is Ownable {
 
 	EthPriceOracleInterface private oracleInstance;
 	address private oracleAddress;
+	mapping(uint256=>bool) myRequests;
 
 	event newOracleAddressEvent(address oracleAddress);
+	event ReceivedNewRequestIdEvent(uint256 id);
 
-	function setOracleInstanceAddress(address _oracleInstanceAddress) public onlyOwner {
+	function setOracleInstanceAddress (address _oracleInstanceAddress) public onlyOwner {
 		oracleAddress = _oracleInstanceAddress;
 		oracleInstance = EthPriceOracleInterface(oracleAddress);
-		newOracleAddressEvent(oracleAddress);
+		emit newOracleAddressEvent(oracleAddress);
+	}
+
+	function updateEthPrice () public {
+		uint256 id = oracleInstance.getLatestEthPrice();
+		myRequests[id] = true;
+		ReceivedNewRequestIdEvent(id);
 	}
 }
